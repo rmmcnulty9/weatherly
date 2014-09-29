241fbac7342e32cc38f1daba9429068c4e10759c
Ryan McNulty <rmmcnulty9@gmail.com>
2014-09-28 21:46:30 -0700

    Replace init values for current weather. Fixed bug.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,18 +11,18 @@ angular.module('weatherlyApp')
   .controller('MainCtrl', function ($scope, getCurrentService, getForecastService, kelvinToDegree) {
     $scope.location;
     $scope.initMainData = {
-        humidity: 68,
-        pressure: 1014,
-        temp: 288.9,
-        temp_max: 291.15,
-        temp_min: 286.15,
+        humidity: "-",
+        pressure: "-",
+        temp: "-",
+        temp_max: "-",
+        temp_min: "-",
     };
 
     $scope.initWeatherData = {
-        description: "few clouds",
-        icon: "02n",
+        description: "-",
+        icon: "-",
         id: 801,
-        main: "Clouds"
+        main: "-"
     };
 
     $scope.currentMainData = $scope.initMainData;
@@ -31,8 +31,8 @@ angular.module('weatherlyApp')
     $scope.getWeather = function () {
         getCurrentService($scope.location, function (main, weather) {
 
-            $scope.currentMainData = data.main;
-            $scope.currentWeatherData = data.weather[0];
+            $scope.currentMainData = main;
+            $scope.currentWeatherData = weather;
         });
 
         getForecastService($scope.location, function (data) {
