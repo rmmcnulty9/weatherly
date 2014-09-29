788b846bd4fa681eb33e022473681a11ad461519
Ryan McNulty <rmmcnulty9@gmail.com>
2014-09-28 19:00:36 -0700

    Use the new services.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the weatherlyApp
  */
 angular.module('weatherlyApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, getCurrentService, getForecastService) {
+    $scope.location;
+    $scope.getWeather = function () {
+        getCurrentService($scope.location, function (data) {
+            console.log("current: " + data);
+        });
+
+        getForecastService($scope.location, function (data) {
+            console.log("forecast: " + data);
+        });
+    }
   });
